822203fcea9a1adfe83a85d57bf72f7fb8abcb1a
Antonio Piccolboni <antonio@piccolboni.info>
2016-02-04 17:29:43 -0800

    extract and more efficient separate

--- pkg/R/separate-unite.R
+++ pkg/R/separate-unite.R
@@ -2,19 +2,43 @@ separate_.tbl_HS2 =
   function() {
     stopif(is.integer(sep), "integer separator nor supported yet")
     stopif(
-      !remove || convert || extra != "warn" || fill != "warn",
+      convert || extra != "warn" || fill != "warn",
       "additional arguments not supported yet")
-    select_(
+    tmp =
+      collapse(
+        mutate_(
+          data,
+          .dots = setNames(list(interp(~split(col, sep), col = as.name(col))), "pjezgdwlsd")))
+    tmp =
       mutate_(
-        data,
+        tmp,
         .dots =
           setNames(
-            map(
-              seq_along(into), function(i) interp(~split(col, sep)[j], col = as.name(col), j = i -1)),
-            into)),
-      interp(~-col, col = as.name(col)))}
+            map(seq_along(into), function(i) interp(~pjezgdwlsd[j], j = i - 1)),
+            into))
+    .dots = list(~-pjezgdwlsd)
+    if(remove)
+      .dots = c(.dots, list(interp(~-col, col = as.name(col))))
+    select_(tmp,  .dots = .dots)}
 
 formals(separate_.tbl_HS2) = formals(tidyr::separate_)
+
+extract_.tbl_HS2 =
+  function() {
+      tmp =
+        mutate_(
+          data,
+          .dots =
+            setNames(
+              map(
+                seq_along(into), function(i) interp(~regexp_extract(col, regex, j), col = as.name(col), j = as.integer(i))),
+              into))
+      if(remove)
+        select_(tmp, interp(~-col, col = as.name(col)))
+      else
+        tmp}
+
+formals(extract_.tbl_HS2) = formals(tidyr::extract_)
 
 unite_.tbl_HS2 =
   function(){
