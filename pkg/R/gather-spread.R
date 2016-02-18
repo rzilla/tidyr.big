43abbefd204a5545e9a6f017ed58f2426e048267
Antonio Piccolboni <antonio@piccolboni.info>
2016-02-18 10:01:10 -0800

    brickhouse UDF spread, avoids one join per new col, needs this special init query: Create temporary function collect as  'brickhouse.udf.collect.CollectUDAF'

--- pkg/R/gather-spread.R
+++ pkg/R/gather-spread.R
@@ -11,7 +11,7 @@ gather_.tbl_HS2 =
               .dots =
                 c(
                   setdiff(colnames(data), gather_cols),
-                  setNames(col, key_col),
+                  setNames(list(interp(~col, col = col)), key_col),
                   setNames(
                     list(interp(~col, col = as.name(col))), value_col)))),
         dplyr::union)
@@ -22,7 +22,9 @@ gather_.tbl_HS2 =
 
 formals(gather_.tbl_HS2) = formals(tidyr::gather_)
 
-spread_.tbl_HS2 =
+# we don't support tbl_sql quite yet, but this doesn't need special UDFs so
+# I am going to keep it around
+spread_.tbl_sql =
   function() {
     new_cols = unlist(collect(distinct_(select_(data, key_col))))
     reduce(
@@ -35,14 +37,37 @@ spread_.tbl_HS2 =
         map(
           new_cols,
           function(col)
+            data %>%
+            filter_(
+              .dots = interp(~key_col == col, key_col = as.name(key_col), col = col)) %>%
+            select_(.dots = interp(~-key_col, key_col = as.name(key_col))) %>%
             rename_(
-              select_(
-                filter_(
-                  data,
-                  .dots = interp(~key_col == col, key_col = as.name(key_col), col = col)),
-                .dots =
-                  interp(~-key_col, key_col = as.name(key_col))),
               .dots = setNames(value_col, col)))),
       left_join)}
+
+# this needs the brickouse UDFs
+spread_.tbl_HS2 =
+  function() {
+    other_cols = setdiff(colnames(data), c(key_col, value_col))
+    new_cols = unlist(collect(distinct_(select_(data, key_col))))
+    data %>%
+      group_by_(.dots = other_cols) %>%
+      summarize_(
+        .dots =
+            list(
+              valuemap =
+                interp(
+                  ~collect(key_col, value_col),
+                  key_col = as.name(key_col),
+                  value_col = as.name(value_col)))) %>%
+      transmute_(
+        .dots =
+          c(other_cols,
+            setNames(
+              map(
+                new_cols,
+                function(col)
+                  interp(~valuemap[col], col = col)),
+              new_cols)))}
 
 formals(spread_.tbl_HS2) = formals(tidyr::spread_)
