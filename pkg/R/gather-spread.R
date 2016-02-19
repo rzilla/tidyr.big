002f2240a02f4122f1ad2f946e2a15c2e91c0a0e
Antonio Piccolboni <antonio@piccolboni.info>
2016-02-19 12:44:05 -0800

    efficient gather using brickhouse UDF

--- pkg/R/gather-spread.R
+++ pkg/R/gather-spread.R
@@ -1,4 +1,6 @@
-gather_.tbl_HS2 =
+# we don't support tbl_sql quite yet, but this doesn't need special UDFs so
+# I am going to keep it around
+gather_.tbl_sql =
   function() {
     stopif(convert || factor_key, "Not supported yet")
     tmp =
@@ -15,6 +17,41 @@ gather_.tbl_HS2 =
                   setNames(
                     list(interp(~col, col = as.name(col))), value_col)))),
         dplyr::union)
+    if(na.rm)
+      filter_(tmp, interp(~!is.null(value_col), value_col = as.name(value_col)))
+    else
+      tmp}
+
+formals(gather_.tbl_sql) = formals(tidyr::gather_)
+
+gather_.tbl_HS2 =
+  function() {
+    stopif(convert || factor_key, "Not supported yet")
+    map_call =
+      gather_cols %>%
+      map(function(x) list(x, as.name(x)))  %>%
+      flatten  %>%
+      c(as.name("map"),.)  %>%
+      as.call
+    tmp =
+      data %>%
+      mutate_(
+        pclctsdoud =
+          interp(~explode(map_key_values(map_call)), map_call = map_call)) %>%
+      mutate_(
+        .dots =
+          setNames(
+            list(~get(pclctsdoud, key), ~get(pclctsdoud, value)),
+            c("rqdrqgleiu", "xzcpbifzwp"))) %>%
+      select_(.dots =
+                map(
+                  c(gather_cols, "pclctsdoud"),
+                  function(col) interp(~-col, col = as.name(col)))) %>%
+      rename_(
+        .dots =
+          setNames(
+            list(~rqdrqgleiu, ~xzcpbifzwp),
+            c(key_col, value_col)))
     if(na.rm)
       filter_(tmp, interp(~!is.null(value_col), value_col = as.name(value_col)))
     else
@@ -45,7 +82,10 @@ spread_.tbl_sql =
               .dots = setNames(value_col, col)))),
       left_join)}
 
-# this needs the brickouse UDFs
+formals(spread_.tbl_sql) = formals(tidyr::spread_)
+
+
+# this needs the brickouse UDFs and Create temporary function collect as  'brickhouse.udf.collect.CollectUDAF
 spread_.tbl_HS2 =
   function() {
     other_cols = setdiff(colnames(data), c(key_col, value_col))
