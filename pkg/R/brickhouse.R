bf79a6b5fd23a21c16536b91e5ba45c00a0644bb
Antonio Piccolboni <antonio@piccolboni.info>
2016-03-04 13:59:06 -0800

    extend with brickhouse functions

---
+++ pkg/R/brickhouse.R
@@ -0,0 +1,13 @@
+#brickouse extension
+
+
+
+
+brickhouse.extension =
+  function(src) {
+    add_extension(
+      src,
+      c(map_key_values  = "brickhouse.udf.collect.MapKeyValuesUDF",
+        collect = "brickhouse.udf.collect.CollectUDAF"),
+      Sys.getenv("BRICKHOUSE_JAR"))}
+
